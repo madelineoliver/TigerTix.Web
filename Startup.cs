cd96c3c575dad593ab33cacd6d3c6c0566dcd6ce
brewea <56275753+brewea@users.noreply.github.com>
2022-03-09 11:24:49 -0500

    Got new html working

--- Startup.cs
+++ Startup.cs
@@ -16,13 +16,29 @@ namespace TigerTix.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
+           // app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+
+                endpoints.MapControllerRoute("Default", 
+                    "/{controller}/{action}/{id?}",
+                    new { controller = "App", action = "Index" });
+            });
+
+            if(env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
 
         }
     }
